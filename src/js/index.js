ccb608396bef5b4d84809066f49ffcb717f1fd11
Randy <randy708ochoa@gmail.com>
2023-05-20 15:04:26 -0600

    Header terminado.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
 const menu = document.getElementById("menu")
+const menuItems = document.querySelectorAll(".navegacion .item")
+const header = document.querySelector("header")
 
 function menuMobileToggle() {
-    const header = document.querySelector("header")
     header.classList.toggle("mostrar-menu")
 }
 
+function closeMenu() {
+    header.classList.remove("mostrar-menu")
+}
+
+menuItems.forEach((item) => {
+    item.addEventListener("click", closeMenu)
+})
 menu.addEventListener("click", menuMobileToggle)
