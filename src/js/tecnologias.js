a49e81e51efdfe06849c49eaa679206aafbced30
Randy <randy708ochoa@gmail.com>
2023-06-02 11:31:50 -0600

    Section de tecnologias

---
+++ src/js/tecnologias.js
@@ -0,0 +1,12 @@
+const contenedorImagenes = document.querySelectorAll(".tecnologias__contenedor .img__container")
+
+contenedorImagenes.forEach(figure => {
+    const img = figure.children[0]
+    const figcaption = figure.children[1]
+
+    figcaption.textContent = img.alt
+})
+
+
+
+
