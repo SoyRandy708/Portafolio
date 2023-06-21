4ba4ff1b3d3adc5a2bf70f6e0955e56b8afd0881
Randy <randy708ochoa@gmail.com>
2023-06-21 14:49:16 -0600

    Eliminados scripts de Google Analytics, las redes sociales flotantes las elimine y las agregue al inicio en sobre mi

--- src/js/redes-sociales.js
+++
@@ -1,8 +0,0 @@
-const contenedorImagenes = document.querySelectorAll(".redes-sociales .img__container")
-
-contenedorImagenes.forEach(figure => {
-    const img = figure.children[0]
-    const figcaption = figure.children[1]
-
-    figcaption.textContent = img.alt
-})
